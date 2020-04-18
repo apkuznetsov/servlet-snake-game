8924e37c9ae4ea0d2578e69460fe685a6c572a5a
Andrey <kouznts@ya.ru>
2020-04-18 20:47:17 +0400

    GameServlet

--- src/main/java/GameServlet.java
+++ src/main/java/GameServlet.java
@@ -1,28 +1,19 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
-public class MainServlet extends HttpServlet {
-    private static final String startJsp = "/start.jsp";
-
+@WebServlet("/game")
+public class GameServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        final String startJsp = "/game.jsp";
         RequestDispatcher dispatcher = getServletContext().
                 getRequestDispatcher(startJsp);
         dispatcher.forward(request, response);
-    }
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        super.doPost(req, resp);
     }
 }
